ff2e9b6bc9a4846d32c6311744c4a7bef832bb77
surajhosmani <surajhosmani83@gmail.com>
2025-10-30 16:04:53 +0530

    stuuuu details

--- stuuuu.py
+++ stuuuu.py
@@ -9,3 +9,13 @@ print("Name: ", name)
 print("Age: ", age)
 print("USN: ", usn)
 print("Address: ", address)
+
+
+t_name = input("Enter the name of the teacher: ")
+subject = input("Enter the subject: ")
+course_code = input("Enter the course code: ")
+
+print("\n--- Teacher Details ---")
+print(f"Name: {t_name}")
+print(f"Subject: {subject}")
+print(f"Course Code: {course_code}")
