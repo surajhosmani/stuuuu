d310728a60fd0400fdcdd0800412465e3db5f4dc
surajhosmani <surajhosmani83@gmail.com>
2025-10-30 16:00:56 +0530

    Create stuuuu.py

---
+++ stuuuu.py
@@ -0,0 +1,11 @@
+
+
+name = input("Enter student name: ")
+age = input("Enter student age: ")
+usn = input("Enter student USN: ")
+address = input("Enter student address: ")
+
+print("Name: ", name)
+print("Age: ", age)
+print("USN: ", usn)
+print("Address: ", address)
